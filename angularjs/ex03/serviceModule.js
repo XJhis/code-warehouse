41452011746d1501a20efd9cf604ee88533fa124
RUYU\rysh102 <xiongjuhui@linglie.com>
2017-08-30 13:57:29 +0800

    删除代码

--- angularjs/ex03/serviceModule.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2016/9/22 0022.
- */
-var myModule = angular.module('ngService',[]);//模块名称
-myModule.service('$ss',function () {//$ss为该模块的作用域！；
-    this.say=function () {
-        alert('我是自定义的一个模块！');
-    }
-});
