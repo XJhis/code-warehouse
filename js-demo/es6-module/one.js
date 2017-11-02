d7ab324a7a75807994d43841efe1b40a0e6a076b
RUYU\rysh102 <xiongjuhui@linglie.com>
2017-11-02 14:57:59 +0800

    es6-module

--- js-demo/es6-module/one.js
+++ js-demo/es6-module/one.js
@@ -1,3 +1,26 @@
-export var 	 = '熊炬辉';
+export var 	name = '熊炬辉';
 export var age = 25;
 export var year = 1992;
+
+function foo() {
+	return 666
+}
+
+//抛出默认接口
+export default foo;
+
+// //引入接口时，重命名，后面的是新名字
+// import model as nameFn from 'my-module';
+
+// //引入全部
+// import * as math from 'my-module';
+
+// //符合写法
+// export {name, age} from 'my-module'//等于下面写法
+
+// import {name, age} from 'my-module';
+// export {name, age};
+
+
+
+
