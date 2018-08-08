0336e91193e3fee6fba320245de53c186dd7d1d8
xiongjuhui <xiongjuhui@linglie.com>
2018-08-08 18:23:35 +0800

    node学习

--- node-learn/requestHandlers.js
+++ node-learn/requestHandlers.js
@@ -1,12 +1,13 @@
-var exec = require("child_process").exec;
+// var exec = require("child_process").exec;
 var querystring = require("querystring");
 
+//开始页面
 function start(response, postData) {
     var content = "empty";
     var html = `<!DOCTYPE html>
                     <html lang="en">
                     <head>
-                        <meta charset="UTF-8">
+                        <meta charset="UTF-8" http-equiv="Content-Type" content="text/html">
                         <title>Document</title>
                     </head>
                     <body>
@@ -24,20 +25,21 @@ function start(response, postData) {
     response.end();    
 }
 
+//上传文件
 function upload(response, postData) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write('you asd asd：'+querystring.parse(postData).text);
     response.end();
 }
 
+//404页面
 function notFound(response, postData) {
     response.writeHead(404, {'Content-Type': 'text/plain'});
     response.write(`404:${postData}`);
     response.end();
 }
 
-// exports.start = start;
-// exports.upload = upload;
+
 module.exports = {
     start,
     upload,
--- node-learn/server.js
+++ node-learn/server.js
@@ -3,19 +3,17 @@ var url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-    	var postData = "",
-        	pathname = url.parse(request.url).pathname;
+     var postData = "",
+         pathname = url.parse(request.url).pathname;
 
-        request.setEncoding("utf8");
+        // request.setEncoding("utf8");
 
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("接受数据块：" +
-                postDataChunk + "'.");
+        request.on("data", function(postDataChunk) {
+            postData += postDataChunk;            
         });
 
-        request.addListener("end", function() {
-        	console.log('end')
+        request.on("end", function() {
+            console.log('postData:', postData)
             route(handle, pathname, response, postData);
         });
 
@@ -25,3 +23,45 @@ function start(route, handle) {
 }
 
 exports.start = start
+
+//提交表单数据
+var http = require('http');
+var querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+    //req.url不同则返回的页面不同
+    if ('/' == req.url) {
+        var html = ``
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write([
+            '<form method="post" action="/url">',
+            '<h1>My Form</h1>',
+            '<fieldset>',
+            '<label>Personal Information</label>',
+            '<p>What is your name?</p>',
+            '<input type="text" name="name">',
+            '<button>submit</button>',
+            '</form>'
+        ].join(''));
+        res.end();
+    } else if ('/url' == req.url && req.method == 'POST') {
+        var reqBody = '';
+        req.on('data', function(data) {
+            console.log('data：', data)
+            reqBody += data;
+        });
+        req.on('end', function() { //用于数据接收完成后再获取
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('you have sent a ' + req.method + ' request\n');
+            res.write('<p>Content-Type:' + req.headers['content-type'] + '</p>' +
+                '<p>Data:your name is ' + querystring.parse(reqBody).name + '</p>');
+            res.end();
+        })
+    } else {
+        res.writeHead(404);
+        res.write('Not Found');
+        res.end();
+    }
+}).listen(3000, function() {
+    console.log('server is listening 3000');
+});
