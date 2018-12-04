4583f05f895399c62064447e4577d59bca109451
xiongjuhui <962648162@qq.com>
2018-12-04 22:15:01 +0800

    vue

---
+++ vue-demo/双向数据绑定/js/watcher.js
@@ -0,0 +1,46 @@
+//订阅watcher
+function Watcher(vm, exp, cb) {
+    this.cb = cb;
+    this.vm = vm;
+    this.exp = exp;
+    this.value = this.get(); // 将自己添加到订阅器的操作
+}
+
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.vm.data[this.exp];
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);
+        }
+    },
+    get: function() {
+        Dep.target = this; // 缓存watcher实例
+        var value = this.vm.data[this.exp] // 强制observer里的get方法
+        Dep.target = null; // 释放自己
+        return value;
+    }
+};
+
+//发布订阅模式的发布者
+
+Dep.target = null;
+
+function Dep() {
+    this.subs = [];
+}
+
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();
+        });
+    }
+};
